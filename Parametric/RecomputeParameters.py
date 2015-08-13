690a4563118b05591d724cbc96b60c87737c1180
David-Estevez <david.estevez.fdez@gmail.com>
2015-08-14 00:04:30 +0200

    Add license and author information to all sources

--- Parametric/RecomputeParameters.py
+++ Parametric/RecomputeParameters.py
@@ -1,4 +1,31 @@
-__author__ = 'def'
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------#
+#                                                                       #
+# This file is part of the Parametric Workbench                         #
+#                                                                       #
+# Copyright (C) 2015 Mundo Reader S.L.                                  #
+#                                                                       #
+# Author: David Estévez Fernández <david.estevez@bq.com>                #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program. If not, see <http://www.gnu.org/licenses/>.  #
+#                                                                       #
+#-----------------------------------------------------------------------#
+
+__author__ = "David Estévez Fernández <david.estevez@bq.com>"
+__license__ = "GNU General Public License v3 http://www.gnu.org/licenses/gpl.html"
+
 
 import os
 
